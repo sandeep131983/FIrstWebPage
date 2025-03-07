774b9131c0dc2b3a4836989dc20bded4ad3fd696
sandy131983 <sandeep.yerramareddigari@gmail.com>
2025-03-07 12:05:38 -0500

    Firstwebpageexperiment with desc image and weblink

--- webpage.py
+++ webpage.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import pandas
 
 col1,col2=st.columns(2)
 with col1:
@@ -13,7 +13,22 @@ with col2:
 ###Alternate Method to write text on webpage as below
 ##st.text("I have listed examples below which can be vieweed later")"""
 
-context2="I have listed examples below which can be vieweed later"
+context2="I have listed all the apps that can be use later, you can use any app once everything is developed"
 
 st.write(context2)
+
+col3,empty,col4=st.columns([1.5,0.5,1.5])
+list1=pandas.read_csv("data.csv",sep=";")
+with col3:
+    for index,list2 in list1[0:10].iterrows():
+        st.header(list2["title"])
+        st.write(list2["description"])
+        st.image("images/" + list2['image'] )
+        st.write(f"[App Link]({list2['url']})")
+with col4:
+    for index,list2 in list1[10:20].iterrows():
+        st.header(list2["title"])
+        st.write(list2["description"])
+        st.image("images/" + list2['image'])
+        st.write(f"[App Link]({list2['url']})")
 
